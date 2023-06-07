5c310877308d56a369af53ae4eb0d74df3579300
Philipp G <30627896+phigas@users.noreply.github.com>
2023-06-07 18:48:24 -0300

    execute migrations and add str functions to models

--- habit_tracking/models.py
+++ habit_tracking/models.py
@@ -13,18 +13,27 @@ class Habit(models.Model):
     # maybe add a regex valication so only valid colours are allowed
     
     # maybe a description
+    
+    def __str__(self):
+        return self.name
 
 
 class Day(models.Model):
     date = models.DateField()
     
     # maybe day of the week
+    
+    def __str__(self):
+        return self.date
 
 
-class HabitTracking(model.Model):
+class HabitTracking(models.Model):
     habit = models.ForeignKey(Habit, on_delete=models.CASCADE)
     date = models.ForeignKey(Day, on_delete=models.CASCADE)
     # maybe not delete the data if the habit is deleted so that we can look at statistics later
     amount = models.PositiveSmallIntegerField(validators=[MaxValueValidator(5)]) # jennita
     # probably impractical to do a habit more that five times a day (plus don't know how to do it in ui yet)
     # maybe work with percentages or add a field for goal of this day (because the goal might change)
+    
+    def __str__(self):
+        return f'{self.date}, {self.date}'
